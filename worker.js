58fe737df0dfd23bf04cfaa2f8b1e0411934407b
thalamus-live <panfernan5@gmail.com>
2026-06-10 15:36:48 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -41,8 +41,12 @@ export default {
       headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' },
     });
     const html = await response.text();
-    return new Response(html, {
+  return new Response(html, {
       headers: {
         'Content-Type':  'text/html; charset=utf-8',
         'Cache-Control': 'no-store, no-cache, must-revalidate',
-        'X-Deploy':
+        'X-Deploy':      Date.now().toString(),
+      },
+    });
+  },
+};
